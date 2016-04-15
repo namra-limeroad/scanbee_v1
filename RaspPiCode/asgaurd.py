084c92811fd0945611bdc11536b54f90bd2704b3
Namra Maheshwari <namra@limeroad.com>
2016-04-15 12:30:42 +0530

    Raspbeery Pi code + Some resources

---
+++ RaspPiCode/asgaurd.py
@@ -0,0 +1,22 @@
+import datetime
+import time
+import json
+
+data_file_name = "./Sites/data_file.txt"
+data_complete_token = "200633410"
+print "Welcome to Asguard"
+data_list = []
+while(1):
+	data_file  =  open(data_file_name,"w")
+	data = raw_input("")
+	if(data == data_complete_token):
+			current_time = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+			data_hash = {"time_stamp":current_time, "barcode_data":data_list}
+			data_file.write(json.dumps(data_hash))
+			data_list = []
+			print "Data Loading Complete\n"
+			continue
+	data_list.append(data)
+	
+
+	
